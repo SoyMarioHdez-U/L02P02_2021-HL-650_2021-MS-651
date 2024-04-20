39706838e17d3eed44aba5f8c7f2f1f955d95093
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-04-20 09:39:10 -0600

    Se agregó la vista de Crear Usuario

--- L02P02_2021-HL-650_2021-MS-651/Controllers/HomeController.cs
+++ L02P02_2021-HL-650_2021-MS-651/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace L02P02_2021_HL_650_2021_MS_651.Controllers
             return View();
         }
 
+        public IActionResult CrearUsuario()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
