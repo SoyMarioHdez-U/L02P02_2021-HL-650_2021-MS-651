5c0c1ae58448272a2081688bccd47167ae65dc46
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-04-20 11:23:06 -0600

    ASDA

--- L02P02_2021-HL-650_2021-MS-651/Models/clientes.cs
+++ L02P02_2021-HL-650_2021-MS-651/Models/clientes.cs
@@ -12,7 +12,7 @@ namespace L02P02_2021_HL_650_2021_MS_651.Models
         public string? direccion { get; set; }
         public string? genero { get; set; }
         public int? id_departamento { get; set; }
-        public int? ip_puesto { get; set; }
+        public int? id_puesto { get; set; }
         public string? estado_registro { get; set; }
         public DateTime created_at { get; set; }
         public DateTime? updated_at { get; set; }
