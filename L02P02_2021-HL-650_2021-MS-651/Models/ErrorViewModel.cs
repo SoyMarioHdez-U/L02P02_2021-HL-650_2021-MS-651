6754043cebf1fe046c6abe59255177cd4537a902
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-04-20 08:45:01 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2021-HL-650_2021-MS-651/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2021_HL_650_2021_MS_651.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
