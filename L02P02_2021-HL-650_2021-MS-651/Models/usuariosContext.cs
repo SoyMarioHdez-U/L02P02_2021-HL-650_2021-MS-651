c40cafd00efd9d0fc832d43278e7ebb33c5a721b
MAmonterrosa <rodrigo.monterrosa@catolica.edu.sv>
2024-04-20 08:57:03 -0600

    Context y Db

---
+++ L02P02_2021-HL-650_2021-MS-651/Models/usuariosContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L02P02_2021_HL_650_2021_MS_651.Models
+{
+	public class usuariosContext : DbContext
+	{
+
+		public usuariosContext(DbContextOptions<usuariosContext> options) : base(options) 
+		{
+		
+		
+
+		}
+
+	}
+}
--- L02P02_2021-HL-650_2021-MS-651/Program.cs
+++ L02P02_2021-HL-650_2021-MS-651/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using L02P02_2021_HL_650_2021_MS_651.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<usuariosContext>(options =>
+		options.UseSqlServer(
+				builder.Configuration.GetConnectionString("pruebaDbConnection"))); // Hola
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
