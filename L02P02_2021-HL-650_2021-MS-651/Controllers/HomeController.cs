ff77901d723b08fc711552f7d0f04fe468e28cec
MAmonterrosa <rodrigo.monterrosa@catolica.edu.sv>
2024-04-20 11:32:46 -0600

    aRREGALDO

--- L02P02_2021-HL-650_2021-MS-651/Controllers/HomeController.cs
+++ L02P02_2021-HL-650_2021-MS-651/Controllers/HomeController.cs
@@ -10,48 +10,46 @@ namespace L02P02_2021_HL_650_2021_MS_651.Controllers
 
     {
 
-		private readonly usuariosContext _usuariosContext;
-		public HomeController(usuariosContext usuariosContext)
-		{
-			_usuariosContext = usuariosContext;
-		}
+        private readonly usuariosContext _usuariosContext;
+        public HomeController(usuariosContext usuariosContext)
+        {
+            _usuariosContext = usuariosContext;
+        }
 
 
-		public IActionResult Index()
-		{
+        public IActionResult Index()
+        {
             return View();
         }
 
-			var listaDePuestos = (from p in _usuariosContext.puestos
-								  select p).ToList();
-			ViewData["listadoDePuestos"] = new SelectList(listaDeDepartamentos, "id", "puesto");
 
-			var listadoDeClientes = (from c in _usuariosContext.clientes
-									 join d in _usuariosContext.departamentos on c.id equals d.id
-									 join p in _usuariosContext.puestos on c.id equals p.id
-									 select new
-									 {
-										 nombre = c.nombre + " " + c.apellido,
-										 email = c.email,
-										 departamento = d.departamento,
-										 genero = c.genero,
-										 puesto = p.puesto,
-
-									 }
-									).ToList();
-			ViewData["listadoDeClientes"] = listadoDeClientes;
-
-			return View();
-		}
-
-
-		public IActionResult CrearUsuario(clientes nuevoCliente)
+        public IActionResult CrearUsuario()
         {
-			_usuariosContext.Add(nuevoCliente);
-			_usuariosContext.SaveChanges();
+            var listaDeDepartamentos = _usuariosContext.departamentos.ToList();
+            ViewData["listaDeDepartamentos"] = new SelectList(listaDeDepartamentos, "id", "departamento");
 
-			return RedirectToAction("CrearUsuario");
-		}
+            var listaDePuestos = _usuariosContext.puestos.ToList();
+            ViewData["listaDePuestos"] = new SelectList(listaDePuestos, "id", "puesto");
+
+
+            var listaDeClientes = (from c in _usuariosContext.clientes
+                                   join d in _usuariosContext.departamentos on c.id equals d.id
+                                   join p in _usuariosContext.puestos on c.id equals p.id
+                                   select new
+                                   {
+                                       nombre = c.nombre + " " + c.apellido,
+                                       email = c.email,
+                                       direccion = c.direccion,
+                                       departamento = d.departamento,
+                                       genero = c.genero,
+                                       puesto = p.puesto,
+
+                                   }
+                                    ).ToList();
+            ViewData["listadoDeClientes"] = listaDeClientes;
+
+            return View();
+        }
 
         public IActionResult Privacy()
         {
