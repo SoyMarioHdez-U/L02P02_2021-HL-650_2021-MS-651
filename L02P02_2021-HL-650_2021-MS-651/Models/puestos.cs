65728dabe1adb97807b8d3939c75aac68fae47b3
MAmonterrosa <rodrigo.monterrosa@catolica.edu.sv>
2024-04-20 09:06:39 -0600

    Modelos departamento y puesto

---
+++ L02P02_2021-HL-650_2021-MS-651/Models/puestos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L02P02_2021_HL_650_2021_MS_651.Models
+{
+	public class puestos
+	{
+
+		[Key]
+		public int id { get; set; }
+		public string? puesto { get; set; }
+		public string? estado { get; set; }
+		public DateTime? created_at { get; set; }
+	
+
+
+
+	}
+}
